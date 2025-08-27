3b65bf860a18f12cd96e537218fff123dc3e0c1c
Iwan <iwan.robert.biliec@gmail.com>
2025-08-27 13:40:55 +0200

    New commit 0.2 : Ajout compteur init 0

--- MainForm.cs
+++ MainForm.cs
@@ -3,13 +3,25 @@ using System.Windows.Forms;
 
 namespace WinCounter
 {
+
     public class MainForm : Form
     {
+        private Label labelCount;
         public MainForm()
         {
             Text = "Compteur";
             ClientSize = new Size(400, 220);
             StartPosition = FormStartPosition.CenterScreen;
+
+            labelCount = new Label
+            {
+                Dock = DockStyle.Top,
+                Height = 100,
+                Font = new Font(Font.FontFamily, 36, FontStyle.Bold),
+                TextAlign = ContentAlignment.MiddleCenter,
+                Text = "0"
+            };
+            Controls.Add(labelCount);
         }
     }
 }
